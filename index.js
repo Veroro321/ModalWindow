3a341cf92bea2712783af9be12764208d9a3899e
Veronika Polishuk <veronikapolishuk@Veronikas-MacBook-Pro.local>
2024-01-07 14:21:24 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay =document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+
+const openModal = function(){
+    console.log("button clicked")
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+
+for(let i=0; i < btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('click', 
+    openModal)
+} 
+
+
+const closeModal = function(){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden') 
+}
+
+btnCloseModal.addEventListener('click',closeModal);
+overlay.addEventListener('click', closeModal); 
+
+
+
+
+
+// btnCloseModal.addEventListener('click',function(){
+   // modal.classList.add('hidden')
+   // overlay.classList.add('hidden')
+
+//});
+
+//overlay.addEventListener('click', function (){
+    //modal.classList.add('hidden');
+   // overlay.classList.add('hidden');
+
+  
